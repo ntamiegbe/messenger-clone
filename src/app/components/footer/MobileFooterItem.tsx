c7267521b8ab2307dc4514b87db9a6f65d03b250
Ntami Egbe <ntamiegbe00@gmail.com>
2023-05-19 07:12:51 +0100

    feat: created mobile footer

--- src/app/components/footer/MobileFooterItem.tsx
+++ src/app/components/footer/MobileFooterItem.tsx
@@ -1,19 +1,29 @@
 'use client'
 
-import clsx from "clsx"
-import Link from "next/link"
+import clsx from "clsx";
+import Link from "next/link";
 
-type MobileFooterItemProps = {
+interface MobileFooterItemProps {
     label: string;
     icon: any;
     href: string;
     active?: boolean;
 }
 
-const MobileFooterItem = (props: MobileFooterItemProps) => {
-  return (
-    <div>MobileFooterItem</div>
-  )
-}
+const MobileFooterItem: React.FC<MobileFooterItemProps> = ({
+    label,
+    icon: Icon,
+    href,
+    active,
+}) => {
+    return (
+        <Link href={href} className={clsx("group gap-x-3 text-md leading-6 m-2")}>
+            <div className={clsx("font-semibold text-lightGray hover:text-white hover:bg-lighterBlue py-4 px-8 rounded-lg text-center flex flex-col justify-evenly items-center", active && "bg-lightBlue")}>
+                <Icon className="h-6 w-6"/>
+                {label}
+            </div>
+        </Link>
+    )
+};
 
-export default MobileFooterItem
+export default MobileFooterItem;
